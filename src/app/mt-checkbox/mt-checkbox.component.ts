a7eab9bfd22d0fbea9b03a9306c29ecfdb57d6a8
Matthias Thaler <matthias.thaler@brunnerinformatik.ch>
2020-09-03 09:19:18 +0200

    dd

--- src/app/mt-checkbox/mt-checkbox.component.ts
+++ src/app/mt-checkbox/mt-checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mt-checkbox',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mt-checkbox.component.css']
 })
 export class MtCheckboxComponent implements OnInit {
+  @Input() label: string;
+
 
   constructor() { }
 
