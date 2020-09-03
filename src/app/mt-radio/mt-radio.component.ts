1bd2475a618d795209367f1a99092bdfe0a3e98a
Matthias Thaler <matthias.thaler@brunnerinformatik.ch>
2020-09-03 10:28:06 +0200

    ddd

--- src/app/mt-radio/mt-radio.component.ts
+++ src/app/mt-radio/mt-radio.component.ts
@@ -12,6 +12,7 @@ import { Component, OnInit, Input } from '@angular/core';
 // }
 
 export class MtRadioComponent implements OnInit {
+  @Input() label: string;
   @Input() items: string[];
   
   constructor() { }
